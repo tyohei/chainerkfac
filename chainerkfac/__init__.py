1bcbd63688030cf5698605ab146d3777eb477fa2
Yohei Tsuji <t.t.t.yohei@gmail.com>
2019-05-30 22:43:21 +0900

    remove utils

--- chainerkfac/__init__.py
+++ chainerkfac/__init__.py
@@ -1,5 +1,4 @@
 from chainerkfac import communicators  # NOQA
 from chainerkfac import optimizers  # NOQA
-from chainerkfac import utils  # NOQA
 
 from chainerkfac.communicators import create_communicator  # NOQA
